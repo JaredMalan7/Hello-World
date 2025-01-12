637f7611805e4087fc02e2a0f52174d5b6f35257
Jared Malan <jared.malan7@gmail.com>
2025-01-11 21:16:21 -0700

    Initial commit: add Hello World - Personal Portfolio

---
+++ helloworld.js
@@ -0,0 +1,5 @@
+const message = "Hello world!";
+const message2 = "I am printing: Hello world! on the console too!";
+
+document.getElementById("message").innerHTML = message;
+console.log(message2);
